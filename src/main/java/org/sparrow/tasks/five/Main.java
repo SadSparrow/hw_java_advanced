25b8d285e4ccef2bebce696da319460a23414007
SadSparrow <77299989+SadSparrow@users.noreply.github.com>
2022-04-26 14:56:10 +0300

    Update Main.java

--- src/main/java/org/sparrow/tasks/five/Main.java
+++ src/main/java/org/sparrow/tasks/five/Main.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
-
+//test
 public class Main {
 
     public static void main(String[] args) {
